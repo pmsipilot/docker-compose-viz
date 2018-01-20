27758c0f0a34c06596bb68141a5beea0dca31952
jubianchi <contact@jubianchi.fr>
2018-01-20 10:28:12 +0100

    chore: Fix deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,8 +4,9 @@ export REPO=pmsipilot/docker-compose-viz
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 
 git reset --hard
+git clean -dfx
 composer install --no-dev --prefer-dist --classmap-authoritative
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -f Dockerfile -t $REPO:$TAG .
 docker push $REPO
